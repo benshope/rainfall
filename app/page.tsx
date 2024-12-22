2eedf0ce90e069d66ee0deaaf07c0892fad36392
Ben Shope <ben@benshope.com>
2024-12-22 15:33:12 -0700

    sldkfj

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       Hello World. <Link href="/about">About</Link>
+      <Image
+        src="/nextjs-github-pages/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+        priority
+      />
     </div>
   );
 }
