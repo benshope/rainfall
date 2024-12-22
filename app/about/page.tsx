00ec471043d257f0685f904543364d9db1341432
Ben Shope <ben@benshope.com>
2024-12-22 15:43:17 -0700

    lskdjf

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <h1>About Page</h1>
+      <h1>About Pageeeee</h1>
       <div>
-        Back to <Link href="/">Home</Link>
+        Back to <Link href="/">Homeee</Link>
       </div>
     </>
   );
